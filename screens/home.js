7042a6b5e3efbdc2a4588fda89e29322a4088034
Maxxwell <maxxwellfirth@gmail.com>
2020-02-03 18:42:52 +0000

    Initial Commit

---
+++ screens/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+    // navigation.push("ReviewDetails");
+  };
+
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home Screen</Text>
+      <Button title="go to review dets" onPress={pressHandler} />
+    </View>
+  );
+}
